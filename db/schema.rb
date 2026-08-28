55b59c4d7b7f3f8d05619b9f76103c7e9b61c054
Matthew Thomas <matt.thomas2803@gmail.com>
2026-08-28 15:39:42 -0400

    added Addresses

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,13 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[8.1].define(version: 2026_08_29_113836) do
+  create_table "addresses", force: :cascade do |t|
+    t.string "city"
+    t.datetime "created_at", null: false
+    t.string "state"
+    t.datetime "updated_at", null: false
+  end
+
   create_table "medication_types", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.string "name"
